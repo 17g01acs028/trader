64d27970ded5bf4e34db46d36b325ee6dc6696cc
17g01acs028 <stevemutioo123@gmail.com>
2024-10-24 15:58:55 +0300

    latest commit

--- src/Features/Authentication/Components/AuthContext.tsx
+++ src/Features/Authentication/Components/AuthContext.tsx
@@ -1,100 +1,106 @@
 import  {createContext, useContext, useEffect, useState} from 'react';
 import {useQuery, useQueryClient} from "@tanstack/react-query";
-import {account} from "../../../lib/appwrite.ts";
-import {Box, LoadingOverlay} from "@mantine/core";
-import {authenticationQueryKeys, Logout} from "../data-access/authedication.ts";
-import {useMantineNotification} from "../../../Components/Notification/useMantineNotification.tsx";
+import { account } from "../../../lib/appwrite.ts";
+import {
+  authenticationQueryKeys,
+  getAccount,
+  Logout,
+} from "../data-access/authedication.ts";
+import { useMantineNotification } from "../../../Components/Notification/useMantineNotification.tsx";
 
 // AuthContext to store the user globally
 const AuthContext = createContext<{
-    user: any;
-    login: (user: any, token: any) => void;
-    logout: () => void;
-    refresh:()=> void;
-    isLoading:boolean;
-    data:any
-    error:any
-    isSuccess:boolean;
-    isAuthenticated: boolean;
-    setUserFC: (user: any) => void;
-    setIsAuthenticated: (isAuthenticated: boolean) => void;
+  user: any;
+  login: (user: any, token: any) => void;
+  logout: () => void;
+  refresh: () => void;
+  isLoading: boolean;
+  data: any;
+  error: any;
+  isSuccess: boolean;
+  isAuthenticated: boolean;
+  setUserFC: (user: any) => void;
+  setIsAuthenticated: (isAuthenticated: boolean) => void;
 } | null>(null);
 
 export const useAuth = () => {
-    const context = useContext(AuthContext);
-    if (!context) throw new Error('useAuth must be used within AuthProvider');
-    return context;
+  const context = useContext(AuthContext);
+  if (!context) throw new Error("useAuth must be used within AuthProvider");
+  return context;
 };
 
-const AuthProvider = ({children}) => {
-    const [user, setUser] = useState(null);
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const queryClient = useQueryClient()
-    const useLogout = Logout();
-    const useClient = useQueryClient()
-    const fetchUser = async () => {
-        const response = await account.get();
-        if (!response?.status) {
-            throw new Error('Failed to fetch user');
-        }
-        return response;
-    };
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const queryClient = useQueryClient();
+  const useLogout = Logout();
+  const getUserAccount = getAccount();
+  const useClient = useQueryClient();
+  const fetchUser = async () => {
+    const response = await account.get();
+    if (!response?.status) {
+      throw new Error("Failed to fetch user");
+    }
+    return response;
+  };
 
-    const {data, error, isLoading, isSuccess} = useQuery({
-        queryKey: authenticationQueryKeys.logIn(),
-        queryFn: fetchUser,
+  const { data, error, isLoading, isSuccess } = getAccount();
+
+  useEffect(() => {
+    if (isSuccess && data) {
+      setUser(data);
+    }
+  }, [data, isSuccess]);
+
+  const setUserFC = (user: any) => {
+    setUser(user);
+  };
+
+  const refresh = () => {
+    useClient.invalidateQueries(authenticationQueryKeys.all);
+  };
+  const login = (user, token) => {
+    setUser(user);
+    setIsAuthenticated(true);
+    localStorage.setItem("authToken", token);
+  };
+
+  const logout = () => {
+    setUser(null);
+    setIsAuthenticated(false);
+    localStorage.removeItem("authToken");
+    useLogout.mutate(undefined, {
+      onSuccess: () => {
+        queryClient.invalidateQueries(authenticationQueryKeys?.all);
+      },
+      onError: () => {
+        useMantineNotification({
+          type: "error",
+          message: "Error Occurred Please Try again",
+        });
+      },
     });
+  };
 
-    useEffect(() => {
-        if (isSuccess && data) {
-            setUser(data);
-        }
-    }, [data, isSuccess])
-
-    if (isLoading) {
-        return (
-            <Box pos={"relative"} style={{
-                width: "100vw",
-                height: "100vh"
-            }}>
-                <LoadingOverlay visible={isLoading} zIndex={1000} overlayProps={{radius: "sm", blur: 2}}/>
-            </Box>
-        );
-    }
-
-    const setUserFC = (user: any) => {
-        setUser(user)
-    }
-
-    const refresh = ()=>{
-        useClient.invalidateQueries(authenticationQueryKeys.all)
-    }
-    const login = (user, token) => {
-        setUser(user);
-        setIsAuthenticated(true);
-        localStorage.setItem('authToken', token);
-    };
-
-    const logout = () => {
-        setUser(null);
-        setIsAuthenticated(false);
-        localStorage.removeItem('authToken');
-        useLogout.mutate(undefined, {
-            onSuccess: () => {
-                queryClient.invalidateQueries(authenticationQueryKeys?.all)
-            },
-            onError: () => {
-                useMantineNotification({type: "error", message: "Error Occurred Please Try again"})
-            }
-        })
-    };
-
-
-    return (
-        <AuthContext.Provider value={{user, error,refresh,data,isLoading,isSuccess,isAuthenticated, setUserFC, login, logout, setIsAuthenticated}}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        error,
+        refresh,
+        data,
+        isLoading,
+        isSuccess,
+        isAuthenticated,
+        setUserFC,
+        login,
+        logout,
+        setIsAuthenticated,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
--- src/Features/Authentication/Components/AuthedicatedContext.tsx
+++ src/Features/Authentication/Components/AuthedicatedContext.tsx
@@ -4,26 +4,26 @@ import { useNavigate} from "@tanstack/react-router";
 
 
 const AuthenticatedComponent = ({children}: { children: React.ReactNode }) => {
-    const {user,refresh,isLoading,data,isSuccess,error} = useAuth();
-    const navigate = useNavigate();
+  const { user, refresh, isLoading, data, isSuccess, error } = useAuth();
+  const navigate = useNavigate();
 
-    // Check if the app is offline
-    if (!navigator.onLine) {
-        return <div>No internet connection. Please try again later.</div>;
+  // Check if the app is offline
+  if (!navigator.onLine) {
+    return <div>No internet connection. Please try again later.</div>;
+  }
+
+  refresh();
+
+  React.useEffect(() => {
+    if (!isLoading && !user?.status && (isSuccess || error)) {
+      if (!data?.status) {
+        navigate({ to: "/auth" });
+      }
     }
+  }, [user, navigate, isLoading, isSuccess, data]);
 
-    refresh()
-
-    React.useEffect(() => {
-        if(!isLoading && !user?.status && (isSuccess || error)){
-            if(!data?.status){
-                navigate({to:"/auth"})
-            }
-        }
-    }, [user,navigate,isLoading,isSuccess,data]);
-
-    // If authenticated, render the children components
-    return <>{children}</>;
+  // If authenticated, render the children components
+  return <>{children}</>;
 };
 
 export default AuthenticatedComponent;
